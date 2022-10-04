36135f68c0f50b32b8e680caf2e2abf34a6e6ce6
unknown <mic.delpapa@stud.uniroma3.it>
2022-10-04 11:45:03 +0200

    First commit

---
+++ history.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Thu Aug 25 16:00:17 2022)---
+runfile('C:/Users/Michele/.spyder-py3/temp.py', wdir='C:/Users/Michele/.spyder-py3')
+runfile('C:/Users/Michele/.spyder-py3/untitled0.py', wdir='C:/Users/Michele/.spyder-py3')
+runcell(0, 'C:/Users/Michele/.spyder-py3/untitled0.py')
+runfile('C:/Users/Michele/.spyder-py3/untitled0.py', wdir='C:/Users/Michele/.spyder-py3')
+%tb
+debugfile('C:/Users/Michele/.spyder-py3/untitled0.py', wdir='C:/Users/Michele/.spyder-py3')
+
+## ---(Tue Sep  6 13:39:16 2022)---
+runfile('C:/Users/Michele/.spyder-py3/AttaccoDES.py', wdir='C:/Users/Michele/.spyder-py3')
+
+## ---(Mon Sep 19 13:22:35 2022)---
+runfile('C:/Users/Michele/.spyder-py3/AttaccoDES.py', wdir='C:/Users/Michele/.spyder-py3')
+
+## ---(Sun Sep 25 15:15:19 2022)---
+runfile('C:/Users/Michele/.spyder-py3/AttaccoDES.py', wdir='C:/Users/Michele/.spyder-py3')
+
+## ---(Mon Oct  3 18:14:17 2022)---
+runfile('C:/Users/Michele/.spyder-py3/AttaccoDES.py', wdir='C:/Users/Michele/.spyder-py3')
